fac0006a3deb94794551171632b1b604bcf00001
karlitos <klitos2589@gmail.com>
2017-01-01 17:37:49 -0600

    Add static todo list (missing stuff)

---
+++ app/components/TodoList.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Todo from "Todo";
+
+export default class TodoList extends React.Component {
+  render() {
+    let {todos} = this.props;
+    const renderTodos = () => {
+      return todos.map((todo) => {
+        return (
+          <Todo key={todo.id} {...todo}/>
+        ) ;
+      });
+    };
+    return (
+      <div>
+        {renderTodos()}
+      </div>
+    );
+  }
+}
