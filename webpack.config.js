2457c9d9c5e61abcec08be782f633b237e75e745
karlitos <klitos2589@gmail.com>
2017-02-26 15:10:12 -0600

    Fix built bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,11 @@ module.exports = {
       '$': 'jquery',
       'jQuery': 'jquery'
     }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
     new webpack.optimize.UglifyJsPlugin({
       compressor: {
         warnings: false
