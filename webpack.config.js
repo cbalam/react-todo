f157049203d28d17e1a7e1394bd61fe2783c8a03
karlitos <klitos2589@gmail.com>
2017-02-26 14:53:07 -0600

    Fix NODE_ENV in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const webpack = require("webpack");
 const path = require("path");
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'production';
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 module.exports = {
   entry: [
